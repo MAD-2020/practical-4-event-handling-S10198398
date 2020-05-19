bfd0d1e43da363e360dabc3c44f71014edf149a4
Jeremiah Apple <jeremiahlong777@yahoo.com.sg>
2020-05-19 19:00:04 +0800

    Completed but literally wanna cry ;-;

--- app/src/main/java/sg/edu/np/WhackAMole/MainActivity.java
+++ app/src/main/java/sg/edu/np/WhackAMole/MainActivity.java
@@ -2,6 +2,7 @@ package sg.edu.np.WhackAMole;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,6 +11,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,20 +25,60 @@ public class MainActivity extends AppCompatActivity {
         - The function nextLevel() launches the new advanced page.
         - Feel free to modify the function to suit your program.
     */
+    private TextView CountTextView;
 
+    private Button Button1;
+    private Button Button2;
+    private Button Button3;
+    private Integer score = 0;
+
+    final String TAG = "WhackAMole 2.0";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CountTextView = (TextView) findViewById(R.id.advancedScoreText);
+        Button1 = (Button) findViewById(R.id.Button1);
+        Button2 = (Button) findViewById(R.id.Button2);
+        Button3 = (Button) findViewById(R.id.Button3);
+        final List<Button> buttonList = new ArrayList<>();
+        buttonList.add(Button1);
+        buttonList.add(Button2);
+        buttonList.add(Button3);
+        setNewMole(buttonList);
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                for(int i=0; i<3; i++) {
+                    if (buttonList.get(i).getId() == v.getId()) {
+                        if(i == 0){
+                            Log.v(TAG, "Button, Left clicked");
+                            doCheck(Button1);
+                        }
+                        else if (i == 1){
+                            Log.v(TAG, "Button, middle clicked");
+                            doCheck(Button2);
+                        }
+                        else if (i == 2){
+                            Log.v(TAG, "Button, right clicked");
+                            doCheck(Button3);
+                        }
+                    }
+                }
 
+                CountTextView.setText(score.toString());
+                setNewMole(buttonList);
+            }
+        };
+        Button1.setOnClickListener(listener);
+        Button2.setOnClickListener(listener);
+        Button3.setOnClickListener(listener);
         Log.v(TAG, "Finished Pre-Initialisation!");
-
-
     }
+
     @Override
     protected void onStart(){
         super.onStart();
-        setNewMole();
         Log.v(TAG, "Starting GUI!");
     }
     @Override
@@ -55,23 +98,69 @@ public class MainActivity extends AppCompatActivity {
         /* Checks for hit or miss and if user qualify for advanced page.
             Triggers nextLevelQuery().
          */
+        if(checkButton.getText() == "*"){
+            Log.v(TAG, "Hit, score added!");
+            score += 1;
+        }
+        else {
+            Log.v(TAG, "Missed, score deducted");
+            score -= 1;
+        }
+
+        if (score >= 10){
+            nextLevelQuery();
+        }
+        CountTextView.setText(String.valueOf(score));
+
     }
 
     private void nextLevelQuery(){
-        /*
-        Builds dialog box here.
-        Log.v(TAG, "User accepts!");
-        Log.v(TAG, "User decline!");
-        Log.v(TAG, "Advance option given to user!");
-        belongs here*/
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle("Warning! Insane Whack-A-Mole incoming!");
+        builder.setMessage("Would you like to advance to advanced mode?");
+
+        builder.setCancelable(true);
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Log.v(TAG, "User accepts!");
+                nextLevel(score);
+
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Log.v(TAG, "User decline!");
+
+
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
+        Log.v(TAG, "Advanced option given to user!");
+
+
     }
 
-    private void nextLevel(){
-        /* Launch advanced page */
+    private void nextLevel(int score){
+
+        Intent levelUpTwo =  new Intent(MainActivity.this, Main2Activity.class);
+        levelUpTwo.putExtra("score", score);
+        startActivity(levelUpTwo);
     }
 
-    private void setNewMole() {
+    private void setNewMole(List<Button> buttonList) {
         Random ran = new Random();
         int randomLocation = ran.nextInt(3);
+        for(int i = 0; i < 3; i++){
+            if(i == randomLocation){
+                buttonList.get(i).setText("*");
+            }
+            else {
+                buttonList.get(i).setText("O");
+            }
+        }
     }
 }
